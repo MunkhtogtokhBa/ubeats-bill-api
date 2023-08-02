a25ce3bfc24d867258419fa7f19015960e32f016
Munkhtogtokh Baasangarav <munkhtogtokh@mezorn.aom>
2023-08-02 13:32:38 +0800

    Update

--- src/midleware/errorHandler.js
+++ src/midleware/errorHandler.js
@@ -1,7 +1,11 @@
 const logger = require('../util/logger')
 
 function handleError (err, req, res, next) {
-  logger.log('error', err.stack)
+//   logger.log('error', err.status)
+//   logger.log('error', err.message)
+//   logger.log('error', err.errors)
+//   logger.log('error', err.stack)
+  logger.log('error', err)
 
   const resp = {
     date: new Date().toISOString(),
