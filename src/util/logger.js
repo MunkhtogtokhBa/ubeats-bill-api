369439d17b24f5ba7d60249503c3dc80fe384779
unknown <munkhtogtokh@mezorn.com>
2023-08-04 10:01:14 +0800

    Update

--- src/util/logger.js
+++ src/util/logger.js
@@ -5,7 +5,7 @@ const customFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${label}] ${level}: ${message}`
 })
 
-function fileName () {
+function dateFormater () {
   const today = new Date()
   const year = today.getFullYear()
   const month = today.getMonth() + 1
@@ -13,24 +13,31 @@ function fileName () {
   return `LOG_${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`
 }
 
-const logger = createLogger({
-  level: 'debug',
-  format: combine(label({ label: 'loging' }), timestamp(), customFormat),
-  // logger method...
-  transports: [
-    // new transports:
-    new transports.File({
-      filename: `logs/${fileName()}.log`
-    }),
-    new transports.Console({
-      level: 'info',
-      format: format.combine(
-        format.colorize(),
-        format.simple()
-      )
-    })
-  ]
-  // ...
-})
+const logger = () => {
+  const dateStr = dateFormater()
+  const filenameWithDate = 'logs/' + dateStr + '.log'
+  return   createLogger({
+    level: 'debug',
+    format: combine(label({ label: 'loging' }), timestamp(), customFormat),
+    // logger method...
+    transports: [
+      // new transports:
+      new transports.File({
+        // filename: `logs/${fileName()}.log`
+        filename: filenameWithDate
+      }),
+      new transports.Console({
+        level: 'info',
+        format: format.combine(
+          format.colorize(),
+          format.simple()
+        )
+      })
+    ]
+    // ...
+  })
+}
+
+
 
 module.exports = logger
