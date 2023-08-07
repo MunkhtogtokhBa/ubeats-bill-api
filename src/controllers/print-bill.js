d60fadc3d9eb0b3a692ecc8d873970b23c62870d
Munkhtogtokh Baasangarav <munkhtogtokh@mezorn.aom>
2023-08-07 18:40:43 +0800

    Update

--- src/controllers/print-bill.js
+++ src/controllers/print-bill.js
@@ -69,13 +69,13 @@ async function sendPrinter (data) {
     PRINTER.println('Огноо: ' + data.today)
     PRINTER.newLine()
     PRINTER.drawLine()
-    PRINTER.setTextQuadArea()
+
     for (const item of data.items) {
       PRINTER.leftRight(item.name.replace(/Ө/g, 'Є').replace(/ө/g, 'є').replace(/Ү/g, 'V').replace(/ү/g, 'v'), item.qty)
     }
 
     PRINTER.drawLine()
-
+    PRINTER.setTextQuadArea()
     PRINTER.println('Нийт хоол: ' + data.totalItems)
     PRINTER.newLine()
     // PRINTER.println('Нийт vнэ: ' + data.totalAmount)
