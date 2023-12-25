98296ac4774ec2d3adcf007892a6864a7af7d17a
unknown <munkhtogtokh@mezorn.com>
2023-12-25 17:18:19 +0800

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.export = {
+    apps: [
+        {
+            name: "print-bill",
+            script: "app.js",
+            instances: 1,
+            exec_mode: 'fork'
+        }
+    ]
+}
